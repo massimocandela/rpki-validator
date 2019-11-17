106cb580f58a7a70b8c493be51a85cc5265e4b48
Massimo Candela <massimo@us.ntt.net>
2019-11-17 20:55:22 +0100

    tests completed

--- tests/test.js
+++ tests/test.js
@@ -37,20 +37,25 @@ var expect = chai.expect;
 var rpki = require("../index");
 var fs = require("fs");
 
-var asyncTimeout = 20000;
+var asyncTimeout = 10000;
 
 const prefixList = JSON.parse(fs.readFileSync("tests/test.json", "utf8"));
 const single = { // It must be in the vrp list
-    prefix: "103.114.191.0/24",
+    prefix: "218.103.58.0/23",
     maxLength: 24,
-    asn: 0
+    asn: "4515"
+};
+const singleNotValidLength = {
+    prefix: "213.74.5.0/26",
+    maxLength: 24,
+    asn: "34984"
 };
 
 const uncovered = {
     prefix: "203.126.124.0/21",
     asn: "9404"
 };
-const first100 = prefixList.slice(0, 10);
+const first100 = prefixList.slice(0, 100);
 const first5000 = prefixList.slice(0, 5000);
 
 describe("Tests", function() {
@@ -120,7 +125,7 @@ describe("Tests", function() {
 
             Promise.all(first100.map(i => rpki.validate(i.prefix, i.asn, false)))
                 .then((list) => {
-                    expect(list.filter(i => i === true).length).to.equal(5000);
+                    expect(list.length).to.equal(100);
                     done();
                 })
         }).timeout(asyncTimeout);
@@ -157,8 +162,7 @@ describe("Tests", function() {
 
         it("single not valid - prefix length", function(done) {
 
-            const prefix = single.prefix.split("/")[0] + "/" + (single.maxLength + 1);
-            rpki.validate(prefix, single.asn, verbose)
+            rpki.validate(singleNotValidLength.prefix, singleNotValidLength.asn, verbose)
                 .then(result => {
                     expect(result).to
                         .containSubset({
@@ -182,12 +186,10 @@ describe("Tests", function() {
         }).timeout(asyncTimeout);
 
         it("multiple mixed", function(done) {
-            const prefix = single.prefix.split("/")[0] + "/" + (single.maxLength + 1);
-
             const list = [
                 rpki.validate(single.prefix, single.asn, verbose),
                 rpki.validate(single.prefix, uncovered.asn, verbose),
-                rpki.validate(prefix, single.asn, verbose),
+                rpki.validate(singleNotValidLength.prefix, singleNotValidLength.asn, verbose),
                 rpki.validate(uncovered.prefix, uncovered.asn, verbose)
             ];
 
@@ -225,113 +227,126 @@ describe("Tests", function() {
 
     });
 
-    describe("preCached", function () {
+    describe("Pre Cached mixed", function () {
 
         const verbose = true;
 
-        it("single valid", function(done) {
-            rpki.validate(single.prefix, single.asn, verbose)
-                .then(result => {
-                    expect(result).to
-                        .containSubset({
-                            valid: true,
-                            reason: null
+        it("single valid - loading VRP list", function(done) {
+            rpki.preCache()
+                .then(() => {
+                    rpki.validate(single.prefix, single.asn, verbose)
+                        .then(result => {
+                            expect(result).to
+                                .containSubset({
+                                    valid: true,
+                                    reason: null
+                                });
+                            done();
                         });
-                    done();
                 });
-
-        }).timeout(asyncTimeout);
+        }).timeout(120000);
 
         it("single not valid - origin", function(done) {
-            rpki.validate(single.prefix, uncovered.asn, verbose)
-                .then(result => {
-                    expect(result).to
-                        .containSubset({
-                            valid: false,
-                            reason: "Not valid origin"
+            rpki.preCache()
+                .then(() => {
+                    rpki.validate(single.prefix, uncovered.asn, verbose)
+                        .then(result => {
+                            expect(result).to
+                                .containSubset({
+                                    valid: false,
+                                    reason: "Not valid origin"
+                                });
+                            done();
                         });
-                    done();
                 });
         }).timeout(asyncTimeout);
 
         it("single not valid - prefix length", function(done) {
 
-            const prefix = single.prefix.split("/")[0] + "/" + (single.maxLength + 1);
-            rpki.validate(prefix, single.asn, verbose)
-                .then(result => {
-                    expect(result).to
-                        .containSubset({
-                            valid: false,
-                            reason: "Not valid prefix length"
+            rpki.preCache()
+                .then(() => {
+                    rpki.validate(singleNotValidLength.prefix, singleNotValidLength.asn, verbose)
+                        .then(result => {
+                            expect(result).to
+                                .containSubset({
+                                    valid: false,
+                                    reason: "Not valid prefix length"
+                                });
+                            done();
                         });
-                    done();
                 });
         }).timeout(asyncTimeout);
 
         it("single not covered", function(done) {
-            rpki.validate(uncovered.prefix, uncovered.asn, verbose)
-                .then(result => {
-                    expect(result).to
-                        .containSubset({
-                            valid: null,
-                            reason: "No ROA available for this prefix"
+            rpki.preCache()
+                .then(() => {
+                    rpki.validate(uncovered.prefix, uncovered.asn, verbose)
+                        .then(result => {
+                            expect(result).to
+                                .containSubset({
+                                    valid: null,
+                                    reason: "No ROA available for this prefix"
+                                });
+                            done();
                         });
-                    done();
                 });
         }).timeout(asyncTimeout);
 
-        it("multiple mixed", function(done) {
-            const prefix = single.prefix.split("/")[0] + "/" + (single.maxLength + 1);
+        it("multiple mixed (4 validations)", function(done) {
 
-            const list = [
-                rpki.validate(single.prefix, single.asn, verbose),
-                rpki.validate(single.prefix, uncovered.asn, verbose),
-                rpki.validate(prefix, single.asn, verbose),
-                rpki.validate(uncovered.prefix, uncovered.asn, verbose)
-            ];
+            rpki.preCache()
+                .then(() => {
+                    const list = [
+                        rpki.validate(single.prefix, single.asn, verbose),
+                        rpki.validate(single.prefix, uncovered.asn, verbose),
+                        rpki.validate(singleNotValidLength.prefix, singleNotValidLength.asn, verbose),
+                        rpki.validate(uncovered.prefix, uncovered.asn, verbose)
+                    ];
 
-            Promise.all(list)
-                .then(([a, b, c, d]) => {
+                    Promise.all(list)
+                        .then(([a, b, c, d]) => {
 
-                    expect(a).to
-                        .containSubset({
-                            valid: true,
-                            reason: null
+                            expect(a).to
+                                .containSubset({
+                                    valid: true,
+                                    reason: null
+                                });
+
+                            expect(b).to
+                                .containSubset({
+                                    valid: false,
+                                    reason: "Not valid origin"
+                                });
+
+                            expect(c).to
+                                .containSubset({
+                                    valid: false,
+                                    reason: "Not valid prefix length"
+                                });
+
+                            expect(d).to
+                                .containSubset({
+                                    valid: null,
+                                    reason: "No ROA available for this prefix"
+                                });
+
+                            done();
                         });
-
-                    expect(b).to
-                        .containSubset({
-                            valid: false,
-                            reason: "Not valid origin"
-                        });
-
-                    expect(c).to
-                        .containSubset({
-                            valid: false,
-                            reason: "Not valid prefix length"
-                        });
-
-                    expect(d).to
-                        .containSubset({
-                            valid: null,
-                            reason: "No ROA available for this prefix"
-                        });
-
-                    done();
                 });
-
         }).timeout(asyncTimeout);
 
+        it("multiple (5000 validations)", function(done) {
 
-        it("multiple - 5000", function(done) {
+            rpki.preCache()
+                .then(() => {
+                    Promise.all(first5000.map(i => rpki.validate(i.prefix, i.asn, false)))
+                        .then((list) => {
+                            expect(list.length).to.equal(5000);
+                            done();
+                        });
+                });
 
-            Promise.all(first5000.map(i => rpki.validate(i.prefix, i.asn, false)))
-                .then((list) => {
-                    expect(list.filter(i => i === true).length).to.equal(5000);
-                    done();
-                })
         }).timeout(asyncTimeout);
 
     });
-
 });
