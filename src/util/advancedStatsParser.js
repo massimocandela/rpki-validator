e5a4bd6884e5bd3a13feea351b2f5da0acbad3d5
Massimo Candela <massimo@us.ntt.net>
2022-10-09 19:43:26 -0400

    add manifest in output

--- src/util/advancedStatsParser.js
+++ src/util/advancedStatsParser.js
@@ -14,12 +14,18 @@ function makeUnique(arr) {
     return Object.values(uniq);
 }
 
+function getManifestKey(file) {
+    return file.split("/").slice(-2).join("-");
+}
+
 export default class MetaIndex {
     ids = {};
     type = {};
     vrps = {};
     ski = {};
     aki = {};
+    manifests = {};
+
     constructor() {}
 
     destroy = () => {
@@ -28,30 +34,37 @@ export default class MetaIndex {
         this.vrps = {};
         this.ski = {};
         this.aki = {};
+        this.manifests = {};
     }
 
-    add = ({type, valid_since, valid_until, file, hash_id, ski, aki, vrps=[]}) => {
-        const item = {type, valid_since, valid_until, file, hash_id, ski, aki};
+    add = ({type, valid_since, valid_until, file, hash_id, ski, aki, vrps=[], manifest}) => {
+        const item = {type, valid_since, valid_until, file, hash_id, ski, aki, manifest: manifest ? getManifestKey(manifest): undefined};
 
         this.ids[hash_id] = item;
 
-        this.type[type] ??= [];
-        this.type[type].push(this.ids[hash_id]);
+        if (type === "manifest") {
+            const manifestKey = getManifestKey(file);
+            this.manifests[manifestKey] = item;
+        } else {
 
-        if (ski) {
-            this.ski[ski] ??= [];
-            this.ski[ski].push(this.ids[hash_id]);
-        }
+            this.type[type] = this.type[type] || [];
+            this.type[type].push(this.ids[hash_id]);
 
-        if (aki) {
-            this.aki[aki] ??= [];
-            this.aki[aki].push(this.ids[hash_id]);
-        }
+            if (ski) {
+                this.ski[ski] = this.ski[ski] || [];
+                this.ski[ski].push(this.ids[hash_id]);
+            }
 
-        for (let vrp of vrps) {
-            const key = getVrpKey(vrp);
-            this.vrps[key] ??= [];
-            this.vrps[key].push(this.ids[hash_id]);
+            if (aki) {
+                this.aki[aki] = this.aki[aki] || [];
+                this.aki[aki].push(this.ids[hash_id]);
+            }
+
+            for (let vrp of vrps) {
+                const key = getVrpKey(vrp);
+                this.vrps[key] = this.vrps[key] || [];
+                this.vrps[key].push(this.ids[hash_id]);
+            }
         }
     }
 
@@ -73,6 +86,40 @@ export default class MetaIndex {
         }
 
         return makeUnique(data.map(i => this._getSki(i.aki)).flat().filter(i => !!i));
+    }
+
+    getStructure = (vrp) => {
+        const tree  = [];
+        let item = makeUnique(this.vrps[getVrpKey(vrp)]);
+
+        while (item.length) {
+            tree.push(item);
+            item = this.getParents(item);
+        }
+
+
+        return tree.flat().map(i => ({...i, manifest: i.manifest ? this.manifests[i.manifest] : undefined}));
+    }
+
+    getFlattenStructure = (vrp) => {
+        const items = this.getStructure(vrp);
+        const out = [];
+        for (let item of items) {
+            if (item.manifest) {
+                out.push(item.manifest);
+                delete item.manifest;
+            }
+            out.push(item);
+        }
+
+        return out;
+    }
+
+    getExpiring = (vrp, expires, now) => {
+        return this.getFlattenStructure(vrp)
+            .filter(i => {
+                return i.valid_until === expires && (!i.valid_since || !now || i.valid_since <= now);
+            });
     }
 
     getChildren = (data) => {
